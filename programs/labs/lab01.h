241fdfa3a4a16a7367c7adabeaea7bb77adabb9a
Doradalberto <isadorasd@al.insper.edu.br>
2022-08-18 09:21:02 -0300

    trying to make it work (L01)

--- programs/labs/lab01.h
+++ programs/labs/lab01.h
@@ -2,8 +2,19 @@
 #include "crazyflie.h"
 
 // Define all LEDs as digital output objects
+DigitalOut L1 (LED_BLUE_L,  !false);
+DigitalOut L2 (LED_GREEN_L, !false);
+DigitalOut L3 (LED_GREEN_R, !false);
+DigitalOut L4 (LED_RED_L, !false);
+DigitalOut L5 (LED_RED_R, !false);
 
 // Define all motors as PWM objects
+PwmOut M1 (MOTOR1);
+PwmOut M2 (MOTOR2);
+PwmOut M3 (MOTOR3);
+PwmOut M4 (MOTOR4);
+
+float tempo = 0.0;
 
 // Main program
 int main()
@@ -17,8 +28,19 @@ int main()
     // Turn off red LEDs indicating motors are disarmed
     
     // Blink green LEDs indicating end of program
+
+    while (tempo <= 5.0){
+        L1 != L1;
+        wait(0.1);
+        tempo += 0.1;
+    }
+
     while(true)
     {
+        //L3 = false;
+        //L4 = false;
+        //L5 = false;
+
         
     }
 }
