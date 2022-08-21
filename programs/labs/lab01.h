3b0359ca83f1de1d6aa5c5c0fb207de84f201970
Stephanie Liu <stephaniel@al.insper.edu.br>
2022-08-21 16:22:53 -0300

    mudei mas n consegui fazer o flash

--- programs/labs/lab01.h
+++ programs/labs/lab01.h
@@ -15,54 +15,51 @@ PwmOut M3 (MOTOR3);
 PwmOut M4 (MOTOR4);
 
 float tempo = 0.0;
+float tempo 2 = 0.0;
 
 // Main program
 int main()
 {
     // Blink blue LED indicating inicialization (5 seconds)
-    
-    // Turn on red LEDs indicating motors are armed
-    
-    // Test all motors with different frequencies (to make different noises)
-    
-    // Turn off red LEDs indicating motors are disarmed
-    
-    // Blink green LEDs indicating end of program
-
     while (tempo <= 5.0){
-
         L1 = !L1;
         wait(0.1);
         tempo += 0.1;
-        
     }
+    // Turn on red LEDs indicating motors are armed
+    L4 = false;
+    L5 = false;
+    wait(1);
 
-    while(true) {
+    // Test all motors with different frequencies (to make different noises)
+    M1.period(1/400);
+    M1 = 0.1;
+    wait(0.1);
+    M1 = 0.0;
 
-        L4 = false;
-        L5 = false;
+    M2.period(1/600);
+    M2 = 0.1;
+    wait(0.1);
+    M2 = 0.0;
 
-        wait(1);
+    M3.period(1/800);
+    M3 = 0.1;
+    wait(0.1);
+    M3 = 0.0;
 
-        M1.period(1/400);
-        M1 = 0.1;
+    M4.period(1/1000);
+    M4 = 0.1;
+    wait(0.1);
+    M4 = 0.0;
+
+    // Turn off red LEDs indicating motors are disarmed
+    L4 = true;
+    L5 = true;
+
+    // Blink green LEDs indicating end of program
+    while (tempo2 <= 2.0){
+        L2 = !L2;
         wait(0.1);
-        M1 = 0.0;
-
-        M2.period(1/600);
-        M2 = 0.1;
-        wait(0.1);
-        M2 = 0.0;
-
-        M3.period(1/800);
-        M3 = 0.1;
-        wait(0.1);
-        M3 = 0.0;
-
-        M4.period(1/1000);
-        M4 = 0.1;
-        wait(0.1);
-        M4 = 0.0;
-
+        tempo2 += 0.1;
     }
 }
